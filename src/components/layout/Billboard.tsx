8e21a2ac3070ea583df57262771cda03cfe08655
maxjn <mohamad.haqnegahdar@gmail.com>
2024-02-09 12:22:07 +0330

    Fix Billboard Bug

--- src/components/layout/Billboard.tsx
+++ src/components/layout/Billboard.tsx
@@ -6,7 +6,7 @@ const Billboard = ({ data }: BillboardProps) => {
     <div className="p-4 sm:p-6 lg:p-8 rounded-xl overflow-hidden">
       <div
         className="rounded-xl relative aspect-square md:aspect-[2.4/1] overflow-hidden bg-cover "
-        style={{ backgroundImage: `url(${data.imageUrl})` }}
+        style={{ backgroundImage: `url(${data?.imageUrl})` }}
       >
         <div className="h-full w-full flex flex-col gap-y-8 justify-center items-center text-center">
           <h3 className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
