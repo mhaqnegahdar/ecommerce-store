775ce30a653db018a103f21ba8237c799a8b0770
maxjn <mohamad.haqnegahdar@gmail.com>
2024-02-29 13:35:02 +0330

    Add New Billboard To mainpage

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -10,7 +10,7 @@ import getProducts from "@/actions/getProducts";
 export const revalidate = 0;
 
 export default async function HomePage() {
-  const billboard = await getBillboard(`1837b96f-9cfe-46b7-a1c8-e1625f665969`);
+  const billboard = await getBillboard(`61cb9587-05c6-4f5e-887d-bd724a5a4fa5`);
   const products = await getProducts({ isFeatured: true });
   return (
     <Container>
